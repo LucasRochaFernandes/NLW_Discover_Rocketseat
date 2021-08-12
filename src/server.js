8d234ccae3bf2c9fb4ce00d83dad2fcdb6b2aba8
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2021-08-12 17:22:34 -0300

    server started

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const route = require('./route')
+const path = require('path')
+
+app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(route)
+
+app.listen(8080, ()=>{console.log("Iniciado")})
