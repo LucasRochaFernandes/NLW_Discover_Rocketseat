e19fa8a31780eaeb8c4affc62bfddb7c0f01edea
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2021-08-17 00:56:09 -0300

    Fixing crashes in room creation

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -4,18 +4,26 @@ const dataBase = require('../db/config')
 module.exports = {
     async create(req, res){
         const db = await dataBase()
-
         const pass = req.body.password
-
+        let roomIdAlreadyExists = false
         let roomId= '';
-        for(let i = 0; i<6; i++){
-            roomId += Math.floor(Math.random() * 10).toString()
-        }
 
+        while(!roomIdAlreadyExists){
+            for(let i = 0; i<6; i++){
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            const  roomsCreated = await db.all(`SELECT id FROM rooms`)
+
+            if(!roomsCreated.some(roomMeet => roomMeet === parseInt(roomId))){
+                roomIdAlreadyExists = true
+            }
+        }
+      
         await db.run(`
         INSERT INTO rooms(id, pass) VALUES (${parseInt(roomId)}, ${pass}) 
         `)
-
+       
         await db.close()
 
         res.redirect(`/room/${roomId}`)
